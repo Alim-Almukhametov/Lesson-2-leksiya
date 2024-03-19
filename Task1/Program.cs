49824adac675521663f4ecac22b07fdc4cf463c9
Alim-Almukhametov <Swain221221@mail.ru>
2024-03-19 23:41:43 +0500

    add Task1AddLeson2

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[6];
+arr[0] = 1;
+arr[1] = 12;
+arr[2] = 13;
+arr[3] = 14;
+arr[4] = 76;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5]{1, 3, 4, 5, 8};
+
+int[] arr3 = {1, 2, 3, 4, 5};
